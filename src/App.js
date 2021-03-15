4479f432cabe4849ffa1ada251675c441d84fbb8
Erica Siegel <siegel.erica@gmail.com>
2021-03-14 22:52:17 -0700

    created header name and description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+// import the React app
 import React from 'react';
+// import the Header component
+import Header from './components/Header';
+// import the About component
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       
     </div>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
+// import the Nav component
+import Nav from '../Nav'
 
 function Header () {
+    return (
+        <div>
+            <header className="flex-row px-3">
+                <h1>Erica Siegel</h1>
+                <div>
+                    {/* <Nav></Nav> */}
+                </div>
+            </header>
+            <div className="flex-row px-5">
+                <h5>Full Stack Web Design</h5>
+            </div>
+        </div>
+
+    )
 
 }
 
