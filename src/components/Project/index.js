adcdbd175354032302664e5bf9e254192f875202
Erica Siegel <siegel.erica@gmail.com>
2021-03-16 15:20:05 -0700

    found path to display images

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 // import the projects array
-import projectInfo from '../../projects.json';
+
 import ProjectCard from '../ProjectCard';
 import Wrapper from '../Wrapper';
 
@@ -11,7 +11,7 @@ function Project() {
         {
             id: 1,
             name: "The Pet Finder",
-            image: "../../../public/assets/images/thedogspot.jpeg",
+            image: "./assets/images/thedogspot.jpeg",
             description: "Discover your favorite dog breeds. Find the breed that is right for you and save them to your favorites.",
             github: "https://github.com/nrhuizar/The-Pet-Rescuer.git",
             link: "https://nrhuizar.github.io/The-Pet-Rescuer/"
@@ -19,7 +19,7 @@ function Project() {
         {
             id: 2,
             name: "myCipe",
-            image: "../../../public/assets/images/mycipe.png",
+            image: "./assets/images/mycipe.png",
             description: "The place to show off your Chef skills and share your recipes",
             github: "https://github.com/naomiilang/myCipe.git",
             link: "https://mycipeteam.herokuapp.com/"
@@ -27,7 +27,7 @@ function Project() {
         {
             id: 3,
             name: "CMs Tech Blog",
-            image: "../../../public/assets/images/techblog.png",
+            image: "./assets/images/techblog.png",
             description: "Connect with other professionals in the Tech field and share your comments on current and new tech.",
             github: "https://github.com/ericasiegel/cms-blog.git",
             link: "https://cms-blog-db.herokuapp.com/"
@@ -35,7 +35,7 @@ function Project() {
         {
             id: 4,
             name: "Work Day Scheduler",
-            image: "../../../public/assets/images/dayscheduler.jpeg",
+            image: "./assets/images/dayscheduler.jpeg",
             description: "An active daily calendar where you can save your schedule by the hour.",
             github: "https://github.com/ericasiegel/daily-calendar.git",
             link: "https://ericasiegel.github.io/daily-calendar/"
@@ -43,7 +43,7 @@ function Project() {
         {
             id: 5,
             name: "Weather Dashboard",
-            image: "../../../public/assets/images/weatherdash.jpeg",
+            image: "./assets/images/weatherdash.jpeg",
             description: "Find your daily and 5-Day forecast in a city around the world.",
             github: "https://github.com/ericasiegel/weather-dashboard.git",
             link: "https://ericasiegel.github.io/weather-dashboard/"
@@ -51,7 +51,7 @@ function Project() {
         {
             id: 6,
             name: "My Team Generator",
-            image: "../../../public/assets/images/images/myteam.png",
+            image: "./assets/images/myteam.png",
             description: "Generate a contact list for your employees.",
             github: "https://github.com/ericasiegel/my-team.git",
             link: "https://ericasiegel.github.io/my-team/dist/index.html"
